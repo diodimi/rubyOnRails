5e69342bd3c30747da90ec267a79ca34f81afb0d
diodimi <diodimi14@gmail.com>
2021-12-28 23:16:48 +0200

    change route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+end
