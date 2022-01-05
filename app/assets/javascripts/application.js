5299b8901861b0bac1ca6748b1dab554f2e6ed0d
nionios <nionios@localhost.localdomain>
2022-01-05 11:45:44 -0500

    Create a style.js file and add js to create posts' style

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,1 @@
+//= require posts/style.js
--- app/assets/javascripts/posts/style.js
+++ app/assets/javascripts/posts/style.js
@@ -1,33 +1,4 @@
-$(document).on('turbolinks:load', function() {
-    if ($(".single-post-card").length) {
-        // set a solid background color style
-        if (mode == 1) {
-            $(".single-post-card").each(function() {
-                $(this).addClass("solid-color-mode");
-                $(this).css('background-color', randomColor());
-            });
-        }
-        // set a border color style
-        else {
-            $(".single-post-card").each(function() {
-                $(this).addClass("border-color-mode");
-                $(this).css('border', '5px solid ' + randomColor());
-            });
-        }	
-    }
-
-
-    $('#feed').on( 'mouseenter', '.single-post-list', function() {
-        $(this).css('border-color', randomColor());	
-    });
-
-    $('#feed').on( 'mouseleave', '.single-post-list', function() {
-        $(this).css('border-color', 'rgba(0, 0 , 0, 0.05)');	
-    });
-
-});
-
-var colorSet = randomColorSet();
+      var colorSet = randomColorSet();
 var mode = Math.floor(Math.random() * 2);
 
 // Randomly returns a color scheme
@@ -48,3 +19,37 @@ function randomColor() {
     var color = colorSet[Math.floor(Math.random() * colorSet.length)];
     return color;
 }
+
+
+
+    if ($(".single-post-card").length) {
+        
+        // set a solid background color style
+        if (mode == 1) {
+            $(".single-post-card").each(function() {
+                $(this).addClass("solid-color-mode");
+                $(this).css('background-color', randomColor());
+            });
+        }
+        // set a border color style
+        else {
+        
+
+            $(".single-post-card").each(function() {
+                $(this).addClass("border-color-mode");
+                $(this).css('border', '5px solid ' + randomColor());
+            });
+        }	
+    }
+
+
+    $('#feed').on( 'mouseenter', '.single-post-list', function() {
+        $(this).css('border-color', randomColor());	
+    });
+
+    $('#feed').on( 'mouseleave', '.single-post-list', function() {
+        $(this).css('border-color', 'rgba(0, 0 , 0, 0.05)');	
+    });
+
+
+
