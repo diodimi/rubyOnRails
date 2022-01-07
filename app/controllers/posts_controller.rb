6d52d5fd0dcd76077a1ca55577fcb9b1e9f12654
diodimi <diodimi14@gmail.com>
2022-01-07 11:19:42 -0500

    Add CSS to the Posts new.html.erb template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
 	before_action :redirect_if_not_signed_in, only: [:new]
+
+
+
   def show
     @post = Post.find(params[:id])
   end
     def hobby
     posts_for_branch(params[:action])
+  end
+
+    def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
   end
 
   def study
@@ -25,7 +43,10 @@ def posts_for_branch(branch)
   format.js { render partial: 'posts/posts_pagination_page' }
 end
 end
-
+  def post_params
+  params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
+end
 def get_posts
   branch = params[:action]
   search = params[:search]
@@ -51,22 +72,6 @@ end
     }).call
   end
 
-    def new
-    @branch = params[:branch]
-    @categories = Category.where(branch: @branch)
-    @post = Post.new
-  end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save 
-      redirect_to post_path(@post) 
-    else
-      redirect_to root_path
-    end
-  end
-  def post_params
-  params.require(:post).permit(:content, :title, :category_id)
-                       .merge(user_id: current_user.id)
-end
+
 end
