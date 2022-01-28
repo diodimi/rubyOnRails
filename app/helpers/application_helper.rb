f46b8e13480cbb5c8b47fe90371a85b41153c173
diodimi <diodimi14@gmail.com>
2022-01-28 11:58:16 -0500

    Generate PagesController. Initialize Home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 require 'navigation_helper.rb'
 
 module ApplicationHelper
+  include Private::MessagesHelper
   include NavigationHelper
   include PostsHelper
   include Private::ConversationsHelper
